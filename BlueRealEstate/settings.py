4796d209252d2e62b883235bc2ff18ef4ab537bf
Daniel18 <daniel.aron1111@gmail.com>
2019-05-09 13:26:55 +0000

    virkni a bakvið register og login

--- BlueRealEstate/settings.py
+++ BlueRealEstate/settings.py
@@ -136,3 +136,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/'
--- log_in/urls.py
+++ log_in/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('', views.log_in_index, name="log_in"),
-
+    path('log_in/', LoginView.as_view(template_name="log_in/log_in.html"), name="log_in"),
+    path('log_out/', LogoutView.as_view(next_page='front_page'), name='log_out'),
     path('register/', views.register_index, name="register"),
 
     path('forgot_password/', views.forgot_password_index, name="forgot_password")
