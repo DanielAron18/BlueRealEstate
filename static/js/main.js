f62a9d833bb3a08c4e6fc0edbafdfebc80dfa495
holmar18 <42944151+holmar18@users.noreply.github.com>
2019-05-09 11:39:49 +0000

    sma fif á nav barið og Byrjun á mobile nav

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+/* Function that displays and hides Mobile navigation using burger icon */
+function showHideNav() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
