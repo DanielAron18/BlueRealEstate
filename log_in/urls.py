33e1c080e59b6f692d8dedfbbe32a30225c26116
Daniel18 <daniel.aron1111@gmail.com>
2019-05-09 11:15:31 +0000

    register og forgot password siður komnar

--- log_in/urls.py
+++ log_in/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.log_in_index, name="log_in"),
-    path('register', views.register_index, name="register"),
+
+    path('register/', views.register_index, name="register"),
+
+    path('forgot_password/', views.forgot_password_index, name="forgot_password")
 ]
--- log_in/views.py
+++ log_in/views.py
@@ -8,4 +8,8 @@ def log_in_index(request):
 
 
 def register_index(request):
-    return render(request, "log_in/register.hrml")
+    return render(request, "log_in/register.html")
+
+
+def forgot_password_index(request):
+    return render(request, "log_in/forgot_password.html")
