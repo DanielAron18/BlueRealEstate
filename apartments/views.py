12ff49e0dd429028d0a1f2f6121260f5c108bcfb
holmar18 <42944151+holmar18@users.noreply.github.com>
2019-05-13 12:00:50 +0000

    Lagaði footerinn, bætti in apartment á index, hægt að klikka á þær

--- apartments/views.py
+++ apartments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from apartments.models import Apartment
+
 
 # Create your views here.
+def index(request):
+    context = {'Apartments': Apartment.objects.all()}
+    return render(request, 'apartments/single_apartment.html', context)
+
+
+def get_apartment_by_id(request, id):
+    return render(request, 'apartments/single_apartment_details.html', {
+        'Apartment': get_object_or_404(Apartment, pk=id)
+    })
