6f297927f4df5a6ce7cef0382cad7ee82ad9f6c9
Daniel18 <daniel.aron1111@gmail.com>
2019-05-13 15:30:55 +0000

    smavægilega breytingar

--- index/views.py
+++ index/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from user.models import User
 # Create your views here.
-
 from apartments.models import Apartment
 
+
 def index(request):
-    context = {'apartments': Apartment.objects.all() }
-    return render(request, "index/front_page.html", context)
+    try:
+        user = User.objects.get(user_id=request.user.id)
+    except:
+        user = None
+    if user != None:
+        return render(request, "index/front_page.html", {
+            'apartments': Apartment.objects.all(),
+            'UserData': user.profilepicture,
+        })
+    else:
+        return render(request, "index/front_page.html", {
+            'apartments': Apartment.objects.all(),
+        })
