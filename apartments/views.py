08c7603c98c5ff3e943e03d7cf082f693127e963
Daniel18 <daniel.aron1111@gmail.com>
2019-05-15 12:50:50 +0000

    bætt við searchistory hvert sinn sem user skoðar ibuð

--- apartments/views.py
+++ apartments/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Func, F, Value
 from django.shortcuts import render, get_object_or_404, redirect
 
 from apartments.forms.add_apartment import AddApartmentForm
@@ -5,12 +6,20 @@ from apartments.models import Apartment
 
 
 # Create your views here.
+from user.models import User
+
+
 def index(request):
     context = {'Apartments': Apartment.objects.all()}
     return render(request, 'apartments/single_apartment.html', context)
 
 
 def get_apartment_by_id(request, id):
+    if request.user.is_authenticated:
+        user = User.objects.get(user_id=request.user.id)
+        if user.searchhistory
+        user.searchhistory.append(id)
+        user.save()
     return render(request, 'apartments/single_apartment_details.html', {
         'Apartment': get_object_or_404(Apartment, pk=id)
     })
