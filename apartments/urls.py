c7ae7a36813c30fb417bb1b069d16c12f53fa06c
onyth <onyth23@gmail.com>
2019-05-16 14:37:36 +0000

    bætti við url fyrir order confirmation

--- apartments/urls.py
+++ apartments/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('<int:id>', views.get_apartment_by_id, name='apartment_details'),
     path('add_apartment', views.add_apartment, name='add_apartment'),
     path('<int:id>/order', views.order, name='order'),
-    path('<int:id>/order_confirmation', views.order_confirmation, name='order_confirmation'),
+    path('<int:id>/order/order_confirmation', views.order_confirmation, name='order_confirmation'),
 ]
